36b4d3917d324b9091781a6b8ae049c218f1aa25
HopkinsKV <hopkinskv@gmail.com>
2021-11-04 15:09:54 -0500

    adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -6,27 +6,58 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#Initialize the accumulator variable. ***This is above the open statement, 
+# because we want to set it to 0 each time we run the file***
+total_votes = 0
+
+# Candidate variables
+candidate_options = []
+# Declare empty dictionary to hold candidate votes
+candidate_votes = {}
+#Declare variables for winning
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
    # Read the file object with the reader function. 
-   # file_reader is referencing the fileobject stored in memory
+   # ***file_reader is referencing the fileobject stored in memory***
     file_reader = csv.reader(election_data)
     
-    # Read and print the header row.
+    # Read the header row (skipping it with next)
     headers = next(file_reader)
-    print(headers)
-    
+
     # Print each row in the CSV file as a list.
     for row in file_reader:
-        print(row)
+        # Iterate through each row and increase accumulator. ***Shorthand for number = number + 1***
+        total_votes += 1
+        # Print Candidate name
+        candidate_name = (row[2])
+        #if uinique add candidate name to list, set votecounter to 0
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
 
 
+for candidate_name in candidate_votes:
+    # Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    # determine winning votes
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        #If true then set winning_count = votes and winning_percent = vote_percentage.
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
 
-#Do analysis here
-#1 the total number of votes
-#2 complete list of candidates that got votes
-#3 the perrcentage of votes per candidates
-#4 total number of votes per candidates
-#5 the winner based on votes
-
-
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
