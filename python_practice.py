a66d9664fff8bdb28a8aa749cb1441827480d5f2
HopkinsKV <hopkinskv@gmail.com>
2021-11-08 09:14:19 -0600

    Deleting python_practice

--- python_practice.py
+++
@@ -1,6 +0,0 @@
-file_to_load = 'Resources/election_results.csv'
-# Open the election results and read the file
-with open(file_to_load) as election_data:
-
-     # To do: perform analysis.
-     print(election_data)
