9bf0173fdf181f8140a2622887e22daf911ad93a
HopkinsKV <hopkinskv@gmail.com>
2021-11-08 11:03:01 -0600

    Print formatting to match image

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -100,7 +100,7 @@ with open(file_to_save, "w") as txt_file:
         # 6c: Calculate the percentage of votes for the county.
         county_vote_percentage = float(countyvotes) / float(total_votes) *100
         county_results = (
-            f"{county_name}: {county_vote_percentage:.1f}% ({countyvotes:,})\n")
+            f"{county_name}: {county_vote_percentage:.1f}% ({countyvotes:,})")
          # 6d: Print the county results to the terminal.
         print(county_results)
          # 6e: Save the county votes to a text file.
@@ -110,12 +110,16 @@ with open(file_to_save, "w") as txt_file:
             largest_turnout = countyvotes
             county_largest = county_name
             county_percentage = county_vote_percentage
- 
+
     # 7: Print the county with the largest turnout to the terminal.
-    print(f"\nLargest Turnout: \n{county_largest}\n-------------------------\n\nCandidate Votes:\n")
+    turnout_results =(
+        f"\n-------------------------\n"
+        f"Largest County Turnout: {county_largest}\n"
+        f"-------------------------\n")
+    print(turnout_results)
 
     # 8: Save the county with the largest turnout to a text file.
-    txt_file.write(f"\nLargest Turnout: {county_largest}\n-------------------------\n\nCandidate Votes:\n")
+    txt_file.write(turnout_results)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
